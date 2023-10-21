25b3ea8beb0a7b7839130ea4c54238112386fd56
BORYS <94471361+BBBORYS@users.noreply.github.com>
2023-10-20 22:23:34 -0500

    Update app.ts

--- app.ts
+++ app.ts
@@ -1,13 +1,11 @@
-// app.ts
+
 import express from 'express';
 import routes from './rutas'; // Importa las rutas desde tu archivo routes.ts
 
 const app = express();
 
-// Middleware para parsear JSON
-app.use(express.json());
 
-// Usar las rutas definidas en el archivo routes.ts
+app.use(express.json());
 app.use('/api', routes);
 
 const PORT = process.env.PORT || 3000;
