8b14023dc51389c8ff7dd02b206546a7c271d5ca
BORYS <94471361+BBBORYS@users.noreply.github.com>
2023-11-17 00:09:25 -0500

    Update create-area.dto.ts

--- src/domain/dtos/area/create-area.dto.ts
+++ src/domain/dtos/area/create-area.dto.ts
@@ -1,15 +1,17 @@
-export class CreateAreaIdDto {
+export class CreateAreaDto {
     private constructor(
       public readonly id : number,
-      public readonly narea: number,
+      public readonly narea: string,
     ){}
-    static create( props: {[key:string]: any} ): [string?, CreateAreaIdDto?]  {
+    static create( props: {[key:string]: any} ): [string?, CreateAreaDto?]  {
   
       const { id, narea} = props;
   
-      if ( !narea) return ['id property is required', undefined];
+      if ( !id ) return ['id property is required', undefined];
+
+      if ( !narea ) return ['id property is required', undefined];
   
   
-      return [undefined, new CreateAreaIdDto(id,narea)];
+      return [undefined, new CreateAreaDto(id,narea)];
     }
   }
